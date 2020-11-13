472d7b806f6e6b6419d6030ec0ee5065e78ccf87
Jack Leung <jack-_-leung@hotmail.com>
2020-11-13 16:41:41 -0500

    refactored lil gbfs

--- gbfs.py
+++ gbfs.py
@@ -5,16 +5,16 @@ import pandas as pd
 class GreedyBFS():
 
     def __init__(self):
-        self.start_state = self.get_start_state("file")
-        self.open_state = Q.PriorityQueue()
-        self.closed_state = []
-        self.visited_state = ()
+        self.start_state = []
+        self.visited_state = []
+        self.closed_state = []  # list of visited states
+        self.open_state = Q.PriorityQueue()  # priority queue ordered by total cost
 
     def get_start_state(self, puzzle_file):
         self.puzzle = pd.read_csv(puzzle_file, sep=" ", header=None)
         self.puzzle_list = []
         for i in range(self.puzzle.ndim+1):
-            list.append(self.puzzle[i].reshape(2,4)) # reshape 1D array(s) to 2x4 (row x col) 2D array
+            self.puzzle_list.append(self.puzzle[i].reshape(2,4)) # reshape 1D array(s) to 2x4 (row x col) 2D array
         return self.puzzle_list
 
     def generate_children(self):
