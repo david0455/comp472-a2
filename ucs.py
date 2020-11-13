190c3eabbe78baad82e944ac2853c3dc8beec07a
David <davidchen_98@hotmail.com>
2020-11-13 00:39:46 -0500

    changed priority queue tuples to list, added checkCost, replaceCost functions

--- ucs.py
+++ ucs.py
@@ -2,14 +2,9 @@ import queue as Q
 import pandas as pd
 import numpy as np
 
-from puzzle_rules import *
+from puzzle_rules import Rules
 
 class UniformCostSearch():
-
-    # get_Start_State is useless
-    # just use np.loadtxt('file', delimiter=' ')
-    # like i did in puzzle_rules or just use it from there, but need to separate each line/puzzle
-    # you do it
 
     def _init_(self):
         self.start_State = []
@@ -17,55 +12,86 @@ class UniformCostSearch():
         self.closed_State = []  # list of visited states
         self.open_State = Q.PriorityQueue()  # priority queue ordered by total cost
 
-    def get_Start_State(self, puzzle_file):
-        self.puzzle = pd.read_csv(puzzle_file, sep=" ", header=None)
-        self.puzzle_list = []
-        for i in range(self.puzzle.ndim+1):
-            list.append(self.puzzle[i].reshape(2,4)) # reshape 1D array(s) to 2x4 (row x col) 2D array
-        return self.puzzle_list
-
-    def get_children(self):
-        print("hello")
-        # TODO: Create method to get children (all possible moves) of node zero
-
-    def calc_cost(self):
-        print("hello")
-        # TODO: Calculate cost of move
+    def get_PQ_Content(self, pq):
+        self.content = []
+        while not pq.empty():
+            self.content.append(pq.get())
         
-    def ucs(self):
-        self.start_State = self.get_Start_State("file") 
-        if len(self.start_State) > 0:
+        return self.content
+
+    def compare_Cost(self, child_cost, child_state, pq):
+        self.content = self.get_PQ_Content(pq) # put priority queue content in list
+
+        if len(self.content) != 0:
+            for cost in self.content:
+                if child_state == cost[2]: # get puzzle state in list
+                    if child_cost < cost[0]: # get cost value in list
+                        return True
+                    else:
+                        return False
+
+    def replace_Cost(self, child_cost, child_state, pq):
+        self.content = self.get_PQ_Content(pq) # put priority queue content in list
+        self.new_pq = Q.PriorityQueue()
+        if len(self.content) != 0:
+            for cost in self.content:
+                if child_state == cost[2]: # get puzzle state in list
+                    if child_cost < cost[0]: # get cost value in list
+                        cost[0] = child_cost
+                        self.new_pq.put(cost)
+                else:
+                    self.new_pq.put(cost)
+        return self.new_pq
+
+
+    def printPath(self):
+        print("hello")
+    
+    # initial_State = self.get_Start_State("file")
+    def ucs(self, initial_State):
+        if len(initial_State) > 0:
 
             # iterate all the puzzles in file
-            for i in range(len(self.start_State)):      
-                self.goal = True # placeholder
-                if self.start_State[i] == self.goal: #if initial state is equal to goal, end ucs
+            for i in range(len(initial_State)):
+                rl = Rules(initial_State[i])
+                if rl.checkGoal(): #if initial state is equal to goal, end ucs
                     return "Already at goal state" 
 
-              # self.open_State.put(cost, moved_tile, curr_puzzle, path) # parent node, priority queue 
-                open_State.put(0, 0, self.start_State[i], [0,0])  # initial-state: (Path-Cost=0, moved_tile=0, curr _puzzle = Starting Puzzle, Path = [0,0])     
+                self.open_State.put([0, 0, initial_State[i], [0,0]])  # initial-state: (Path-Cost=0, moved_tile=0, curr _puzzle = Starting Puzzle, Path = [0,0])     
 
                 while not self.open_State.empty():
-                    self.visited_State = self.open_State.get() #pop first element with lowest cost in priority queue
-
+                    self.cost, self.moved_tile, self.visited_State, self.path = self.open_State.get() #pop first element with lowest cost in priority queue
+                    rl2 = Rules(self.visited_State)
                     # apply goal function
-                    if(self.goal is True):
+                    if rl2.checkGoal():
                         self.closed_State.append(self.visited_State) #put the last visited node in closed list
-                        return self.closed_State, self.open_State #return solution path
+                        self.path.append((self.moved_tile, self.cost))
+                        return self.path #return solution path
                     
                     if self.visited_State not in self.closed_State:
                          self.closed_State.append(self.visited_State) #put visited state in closed state
-                         possible_Moves = self.visited_State.get_children()
-                         for route in possible_Moves:
-                            if (route not in self.closed_State) and not self.open_State:  # check if the child is in closed list or open priority queue
-                                self.open_State.put(route)
+                         self.possible_Moves = rl.generate_moves()
+                         while not self.possible_Moves.empty():
+                            self.child_cost, self.child_moved_tile, self.child_State = self.possible_Moves.get()
+                            if (self.child_State not in self.closed_State) and not self.open_State:  # check if the child is in closed list or open priority queue
+                                self.path.append(self.moved_tile, self.cost)
+                                self.open_State.put([self.child_cost, self.child_moved_tile, self.child_State, self.path])
                             elif self.open_State: # if the child in priority queue has higher PATH-COST than this child, replace it
-                                if route.cost < self.open_State.cost: # TODO: find a way to get cost from priority queues
-                                    self.open_State.get(index)
-                                    self.open_State.put(route)
+                                if self.compare_Cost(self.child_cost, self.child_State, self.open_State): 
+                                    self.open_State = self.replace_Cost(self.child_cost, self.child_State, self.open_State)
 
-                
+
         return None
-                    
-                    
+
+def get_Start_State(puzzle_file):
+    input_file = np.loadtxt(puzzle_file, delimiter=' ')
+    puzzle_list = []
+    for i in range(input_file.ndim+1):
+        list.append(input_file[i].reshape(2,4)) # reshape 1D array(s) to 2x4 (row x col) 2D array
+    return puzzle_list
+
+def main():
+    initial_States = get_Start_State("samplePuzzles.txt")
+    solve = UniformCostSearch()
+    solve.ucs(initial_States)
 
