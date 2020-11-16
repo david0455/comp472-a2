2a148d4f2686a897ffc07f6b5686c144942b10db
David <davidchen_98@hotmail.com>
2020-11-16 16:14:06 -0500

    added timer for function execution time limit

--- astar.py
+++ astar.py
@@ -4,6 +4,7 @@ import numpy as np
 import time
 import copy
 
+
 from puzzle_r import check_goal, generate_children
 
 class AStar():
@@ -20,17 +21,7 @@ class AStar():
         else:
             self.h = 1
         return self.h
-    
-    # Checks if current_child state is in open_queue or closed list
-    def is_in(self, elem, arr):
-        deep_copy = copy.deepcopy(arr)
-        state = list(deep_copy)
-        inside = False
-        for i in range(len(state)):
-            for j in range(len(state)):
-                if(elem == state[i][j]):
-                    inside = True
-        return inside
+
     
     def compare_Cost(self, child_cost, child_state, pq):
         if len(pq) != 0:
@@ -60,20 +51,20 @@ class AStar():
         return self.f
 
     def astar(self, initial_state):
-        self.start = time.time()
-
+        start = time.time()
+        
         self.open_state.append([self.h0(initial_state), 0, self.h0(initial_state), initial_state, list([0, self.h0(initial_state), initial_state])]) # initial-state: (f(n), g(n), h(n), curr _puzzle = Starting Puzzle, Path = [tile,cost, current_puzzle])  
         # self.open_state.append([self.h0(initial_state), 0, self.h0(initial_state), initial_state]) # initial-state: (f(n), g(n), h(n), curr _puzzle = Starting Puzzle, Path = [tile,cost, current_puzzle])
         
-        while len(self.open_state) > 0:
+        while (len(self.open_state) > 0):
 
             self.open_state.sort(key=lambda x: x[0])
             self.curr_f, self.curr_g, self.curr_h, self.current_puzzle, self.path = self.open_state.pop(0) # pop lowest cost move from open queue
             #self.curr_f, self.curr_g, self.curr_h, self.current_puzzle = self.open_state.pop(0) # pop lowest cost move from open queue
 
             if check_goal(self.current_puzzle): # Check if current puzzle state is goal state
-                self.end = time.time()
-                execution_time = self.end - self.start
+                end = time.time()
+                execution_time = end - start
                 self.closed_state.append((self.current_puzzle))
                 return print("Solution found in", execution_time, "sec", self.current_puzzle)
             
@@ -104,6 +95,10 @@ class AStar():
                         #self.open_state.append([child_f, total_cost, child_h, self.child_puzzle_state])
             
             self.closed_state.append([self.curr_f, self.curr_g, self.curr_h, self.current_puzzle])
+            temp_time = time.time()
+            if (temp_time - start) > 60:
+                print("No Solution")
+                break
         
         return "No Solution"
 
@@ -119,9 +114,8 @@ def get_Start_State(puzzle_file):
 def main():
     initial_States = get_Start_State("samplePuzzles.txt")
     solve = AStar()
-    for i in range(initial_States):
-        #solve.astar(initial_States[i].tolist())
-        solve.astar(initial_States[0].tolist())
+    for i in range(len(initial_States)):
+        solve.astar(initial_States[i].tolist())
 
 
 if __name__ == '__main__':
